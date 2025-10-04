19ff4025faf4fa10953f49df4fb4697e71af3053
Wesam <wesamsoulaiman@gmail.com>
2025-10-04 04:59:13 +0200

    initial commit

---
+++ backend/src/routes/auth.route.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const router = express();
+
+router.get("/signup", (req, res) => {
+  res.send("singup");
+});
+router.get("/login", (req, res) => {
+  res.send("login");
+});
+router.get("/logout", (req, res) => {
+  res.send("logout");
+});
+
+export default router;
