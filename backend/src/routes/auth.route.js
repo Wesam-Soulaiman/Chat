e3626dba19f77be29dde5fb7c5d5f1a32ea34c78
Wesam <wesamsoulaiman@gmail.com>
2025-10-05 16:34:30 +0200

    login logout

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,14 +1,10 @@
 import express from "express";
-import { signup } from "../controllers/auth.controller.js";
+import { signup, login, logout } from "../controllers/auth.controller.js";
 
 const router = express();
 
 router.post("/signup", signup);
-router.get("/login", (req, res) => {
-  res.send("login");
-});
-router.get("/logout", (req, res) => {
-  res.send("logout");
-});
+router.post("/login", login);
+router.post("/logout", logout);
 
 export default router;
