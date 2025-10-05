219d27f4b8a7fdf86a3daecedf6a4095698565e6
Wesam <wesamsoulaiman@gmail.com>
2025-10-06 01:50:50 +0200

    message routes

---
+++ backend/src/controllers/message.controller.js
@@ -0,0 +1,85 @@
+import cloudinary from "../lib/cloudinary.js";
+import Message from "../models/Message.js";
+import User from "../models/User.js";
+
+export const getAllContacts = async (req, res) => {
+  try {
+    const loggedInUserId = req.user._id;
+    const filteredUsers = await User.find({
+      _id: { $ne: loggedInUserId },
+    }).select("-password");
+    res.status(200).json(filteredUsers);
+  } catch (error) {
+    console.log("Error in getAllContacts:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const getChatPartners = async (req, res) => {
+  try {
+    const loggedInUserId = req.user._id;
+    const messages = await Message.find({
+      $or: [{ senderId: loggedInUserId }, { recipientId: loggedInUserId }],
+    });
+    const chatPartnerIds = [
+      ...new Set(
+        messages.map((msg) =>
+          msg.senderId.toString() === loggedInUserId.toString()
+            ? msg.recipientId.toString()
+            : msg.senderId.toString()
+        )
+      ),
+    ];
+
+    const chatPartners = await User.find({
+      _id: { $in: chatPartnerIds },
+    }).select("-password");
+
+    res.status(200).json(chatPartners);
+  } catch (error) {
+    console.error("Error in getChatPartners: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const getMessagesByUserId = async (req, res) => {
+  try {
+    const loggedInUserId = req.user._id;
+    const { id: userToChatId } = req.params;
+    const messages = await Message.find({
+      $or: [
+        { senderId: loggedInUserId, recipientId: userToChatId },
+        { senderId: userToChatId, recipientId: loggedInUserId },
+      ],
+    });
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log("Error in getMessages controller: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const senderId = req.user._id;
+    const { id: recipientId } = req.params;
+
+    let imageUrl;
+    if (image) {
+      const uploadRes = await cloudinary.uploader.upload(image);
+      imageUrl = uploadRes.secure_url;
+    }
+    const message = new Message({
+      senderId,
+      recipientId,
+      text,
+      image: imageUrl,
+    });
+    await message.save();
+    res.status(200).json(message);
+  } catch (error) {
+    console.log("Error in sendMessage controller: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,9 +1,19 @@
 import express from "express";
+import {
+  getAllContacts,
+  getMessagesByUserId,
+  sendMessage,
+  getChatPartners,
+} from "../controllers/message.controller.js";
+import { protectRoute } from "../middlewares/auth.middleware.js";
+import { arcjetProtection } from "./../middlewares/arcjet.middleware.js";
 
 const router = express();
+router.use(arcjetProtection, protectRoute);
 
-router.get("/send", (req, res) => {
-  res.send("send");
-});
+router.get("/contacts", getAllContacts);
+router.get("/chats", getChatPartners);
+router.get("/:id", getMessagesByUserId);
+router.post("/send/:id", sendMessage);
 
 export default router;
